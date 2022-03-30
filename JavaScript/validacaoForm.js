a8c67abc9b16734b05680d1e69710db5b865ee5d
Vinicius Almeida <vinicius.mansao@gmail.com>
2022-03-30 00:22:17 -0300

    Iniciada a validação do formulário mais simples, como apontar o erro no evento submit, falta validação do e-mail e validação quando usuário passa para o próximo input.

---
+++ JavaScript/validacaoForm.js
@@ -0,0 +1,63 @@
+
+ const form = document.getElementById('form')
+ const nome = document.getElementById('nome')
+ const email = document.getElementById('email')
+ const assunto = document.getElementById('assunto')
+ const mensagem = document.getElementById('mensagem')
+
+ form.addEventListener('submit', (e) => {
+       e.preventDefault()
+
+       checkInputs()
+ })
+
+ function checkInputs() {
+       const nomeValue = nome.value.trim()
+       const emailValue = email.value.trim()
+       const assuntoValue = assunto.value.trim()
+       const mensagemValue = mensagem.value.trim()
+
+       if (nomeValue === '') {
+          validandoErro(nome, 'preencha esse campo');
+       } else if (nomeValue.length < 3) {
+        validandoErro(nome, 'O nome deve ter mais que 3 caracteres');
+       } else {
+          validandoSucesso(nome)
+       }
+
+       if (emailValue === '') {
+          validandoErro(email, 'preencha esse campo');
+       } else {
+          validandoSucesso(email)
+       }
+
+       if (assuntoValue === '') {
+        validandoErro(assunto, 'preencha esse campo');
+       } else if (nomeValue.length < 3) {
+           validandoErro(nome, 'Digite um assunto válido');
+       }    else {
+        validandoSucesso(assunto)
+       }
+
+       if (mensagemValue === '') {
+        validandoErro(mensagem, 'preencha esse campo');
+       } else {
+        validandoSucesso(mensagem)
+       }
+ }
+
+ function validandoErro(input, message) {
+      const formControl = input.parentElement;
+      const small = formControl.querySelector('small')
+
+      small.innerText = message;
+
+      formControl.className = 'form-control error';
+ }
+
+ function validandoSucesso(input) {
+      const formControl = input.parentElement;
+
+      formControl.className = 'form-control success';
+
+ }
